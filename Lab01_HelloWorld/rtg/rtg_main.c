8832404181b49e0cb5419e0c2b72cd83fd334c37
DanDdl <dangladste@gmail.com>
2024-01-09 19:19:22 +0200

    Lab01 + gitigore

---
+++ Lab01_HelloWorld/rtg/rtg_main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+#include "usart.h"
+#include "rtg_main.h"
+
+void rtg_main(void) {
+	printf("Hello world\r\n");
+}
